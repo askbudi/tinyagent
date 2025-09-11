acc5cb42044e567feb516feb97055bde488c1205
Mahdiyar <rapid2share@gmail.com>
2025-09-11 00:43:32 -0400

    Better Build

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
-python3 -m build
+# Activate conda environment
+source ~/.bash_profile && conda activate vibe_cnx
+
+# Install build dependencies if not present
+pip install --upgrade build twine
+
+# Clean previous builds
+rm -rf dist/ build/ *.egg-info/
+
+# Build the package
+python -m build
+
+# Upload to PyPI (requires proper authentication)
 twine upload dist/*
